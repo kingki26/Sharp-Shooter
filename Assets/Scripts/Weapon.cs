2dd3849cffda294f6764351e0c9e8c58cc4246a8
KienVu <kienvu26092k4@gmail.com>
2026-08-24 01:00:21 +0700

    Shoot effect

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] int damageAmount = 1;
 
     StarterAssetsInputs starterAssetsInputs;
@@ -24,6 +25,7 @@ public class Weapon : MonoBehaviour
             return;
         }
 
+        muzzleFlash.Play();
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
