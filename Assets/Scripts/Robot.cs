f5727a1cf8a8074086705b8abdf6ec83a4524863
KienVu <kienvu26092k4@gmail.com>
2026-08-23 22:23:55 +0700

    Initial Project

---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,25 @@
+using StarterAssets;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Robot : MonoBehaviour
+{
+    FirstPersonController player;
+    NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void Start()
+    {
+        player = FindAnyObjectByType<FirstPersonController>();
+        
+    }
+
+    
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
