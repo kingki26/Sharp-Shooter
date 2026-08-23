90c21558c405fe27aa970948ab274624d3097562
KienVu <kienvu26092k4@gmail.com>
2026-08-24 00:11:17 +0700

    shoot logic, enemyHealth

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,14 +1,36 @@
+using StarterAssets;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-       
+    [SerializeField] int damageAmount = 1;
+
+    StarterAssetsInputs starterAssetsInputs;
+
+    void Awake()
+    {
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();    
+    }
     void Update()
     {
-        RaycastHit hit;
-        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        HandleShoot();
+
+    }
+
+    private void HandleShoot()
+    {
+        if (!starterAssetsInputs.shoot)
         {
-            Debug.Log(hit.collider.name);
+            return;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        {
+            EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+            enemyHealth?.TakeDamge(damageAmount);
+            starterAssetsInputs.ShootInput(false);
+
         }
     }
 }
