204b884365714c2ea62d30230c0c372e12760d1c
KienVu <kienvu26092k4@gmail.com>
2026-08-24 17:23:42 +0700

    Gun Pickup

--- Assets/Scripts/ActiveWeapon.cs
+++ Assets/Scripts/ActiveWeapon.cs
@@ -29,6 +29,11 @@ public class ActiveWeapon : MonoBehaviour
         HandleShoot();
     }
 
+    public void SwitchWeapon(WeaponSO weaponSO)
+    {
+        Debug.Log("Player picked up" + weaponSO.name);
+    }
+
     private void HandleShoot()
     {
         if (!starterAssetsInputs.shoot)
