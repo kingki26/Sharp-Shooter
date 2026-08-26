d72ccd9ddee57b9801b6e1d8d2aaf260761c7315
KienVu <kienvu26092k4@gmail.com>
2026-08-26 11:15:14 +0700

    Base Pickup

--- Assets/Scripts/Pickup/Pickup.cs
+++ Assets/Scripts/Pickup/Pickup.cs
@@ -1,17 +1,24 @@
 using UnityEngine;
 
-public class WeaponPickup : MonoBehaviour
+public abstract class Pickup : MonoBehaviour
 {
-    [SerializeField] WeaponSO weaponSO;
+    [SerializeField] float rotationSpeed = 100f;
 
     const string PLAYER_STRING = "Player";
+
+    private void Update()
+    {
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(PLAYER_STRING))
         {
             ActiveWeapon activeWeapon = other.GetComponentInChildren<ActiveWeapon>();
-            activeWeapon.SwitchWeapon(weaponSO);
+            OnPickup(activeWeapon);
             Destroy(this.gameObject);
         }
     }
+
+    protected abstract void OnPickup(ActiveWeapon activeWeapon);
 }
