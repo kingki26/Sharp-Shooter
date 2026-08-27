b39b4125f55ff5b9330ab040b5bcb21b95d545b0
KienVu <kienvu26092k4@gmail.com>
2026-08-27 16:44:06 +0700

    Win condition

--- Assets/Scripts/Enemies/EnemyHealth.cs
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -6,10 +6,17 @@ public class EnemyHealth : MonoBehaviour
     [SerializeField] int startingHealth = 3;
 
     int currentHealth;
+    GameManager gameManager;
 
     void Awake()
     {
         currentHealth = startingHealth;
+    }
+
+    void Start()
+    {
+        gameManager = FindAnyObjectByType<GameManager>();
+        gameManager.AdjustEnemiesLeft(1);
     }
 
     public void TakeDamge(int amount)
@@ -18,6 +25,7 @@ public class EnemyHealth : MonoBehaviour
 
         if(currentHealth <= 0)
         {
+            gameManager.AdjustEnemiesLeft(-1);
             SelfDestruct();
         }
     }
